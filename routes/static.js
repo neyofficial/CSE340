64e2865a8f62a835704c5ef15258c7d397a62b36
NEY <almighty.ney@gmail.com>
2026-03-14 21:37:13 +0000

    W01_v4

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const path = require("path")
 
-// Serve static files
+// Serve static files from the public folder
 router.use(express.static("public"))
 
 // Home route
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.ejs"))
+  res.render("index")
 })
 
 module.exports = router
-
