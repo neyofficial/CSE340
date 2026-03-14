75851dd8e12e16d4ee0d7b757f9bb46f5a41d717
NEY <almighty.ney@gmail.com>
2026-03-14 22:27:24 +0000

    w01_v7

--- controllers/basecontroller.js
+++ controllers/basecontroller.js
@@ -4,6 +4,9 @@ const baseController = {}
 
 baseController.buildHome = async function (req, res, next) {
   const nav = await utilities.getNav()
+  exports.buildHome = async function (req, res) {
+  res.render("index", { title: "Home" })
+}
   res.render("index", {
     title: "Home",
     nav,
